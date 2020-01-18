27a83054b30e703abe1b692d4b075986fc35f474
althea834 <59947686+althea834@users.noreply.github.com>
2020-01-18 18:44:57 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <AuthInfo />
+
+      <ul>
+        <li><Link to="/public">Public Page</Link></li>
+        <li><Link to="/protected">Protected Page</Link></li>
+      </ul>
+
+      <Switch>
+        <Route path="/login">
+          <SignInPage />
+        </Route>
+        <Route path="/public">
+          <PublicPage />
+        </Route>
+        <PrivateRoute path="/protected">
+          <ProtectedPage />
+        </PrivateRoute>
+      </Switch>
+      </div>
+    </Router>
+  )
+}
+
+const simpleAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    simpleAuth.isAuthenticated = true;
+    setTimeout(cb, 50);
+  },
+  signout(cb) {
+    simpleAuth.isAuthenticated = false;
+    setTimeout(cb, 50);
+  }
+}
+
+function AuthInfo() {
+  let history = useHistory();
+
+  return simpleAuth.isAuthenticated ? (
+    <p>
+      Welcome!
+    <button onClick={() => {
+        simpleAuth.signout(() => history.push("/"));
+      }}
+      >
+        Sign out
+    </button>
+    </p>
+  ) : (
+      <p>You are not signed in.</p>
+    );
+}
+
+
+function PublicPage() {
+  return <p>public</p>;
+}
+
+function ProtectedPage() {
+  return <p>protected</p>;
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        simpleAuth.isAuthenticated ? (
+          children
+        ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+      }
+    />
+  );
+}
+
+
+function SignInPage() {
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  let logIn = () => {
+    simpleAuth.authenticate(() => history.replace(from));
+  }
+
+  return (
+    <div>
+      <p>You must sign in to view the page at {from.pathname}</p>
+      <div>
+        <input ref='username' type="text" defaultValue='username' />
+      </div>
+      <div>
+        <input ref='password' type="password" defaultValue='password' />
+      </div>
+      <button styleName='btn' onClick={logIn}>
+      Sign In
+      </button>
+    </div>
+  );
+}
+
+export default App;
